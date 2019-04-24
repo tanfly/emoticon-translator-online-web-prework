e95e102482d412a2da43d7be9ced11cae2fda549
Stella McEwen <mcewen.stella@gmail.com>
2019-04-24 22:33:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(file, emoticon)
   library = load_library(file)
   if library["get_emoticon"][emoticon] == nil 
-    returns 
+    return "Sorry, that emoticon is not known"
 
 def get_japanese_emoticon
   # code goes here
